fb730d87417fece6d837b839398a94bb97b3502f
Rich Daley <git@richd.me>
2012-07-21 18:32:52 +0100

    Import all of current CV. Lots of styling needed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	require 'digest/md5'
+	
+	def gravatar(email, size=80, options={})
+		hash = Digest::MD5.hexdigest(email)
+		image_tag "http://www.gravatar.com/avatar/#{hash}?s=#{size}", options
+	end
+	
+	def sections
+		%w{profile skills employment education interests references}
+	end
+	
+	def section_header(section)
+		{
+			'employment' => 'Employment history',
+			'interests' => 'Interests & other achievements'
+		}[section] || section.capitalize
+	end
+	
 end
