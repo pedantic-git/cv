d79e393217a379e6b1cf9cc4bb00f50c0f218c15
Rich Daley <git@richd.me>
2012-07-21 15:35:06 +0100

    Initial empty bootstrap app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cv::Application.config.secret_token = '45f1f5deadd5ed3b65ff2eecc3dbc56aa2e2ba61f09eeaedb17c583cca175fff2f812964ebfcd3ac721e800e1c0de36b5c40bc58521a93ccc3e5e6cc65ed88e1'
